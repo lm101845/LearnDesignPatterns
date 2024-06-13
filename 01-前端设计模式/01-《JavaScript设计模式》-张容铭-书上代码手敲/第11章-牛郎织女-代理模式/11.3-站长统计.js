2f1aa8f92b666c112b5b789a3b332c5ba8c733a2
liming <1018457683@qq.com>
2024-06-13 12:12:49 +0800

    第11章看完

---
+++ 01-前端设计模式/01-《JavaScript设计模式》-张容铭-书上代码手敲/第11章-牛郎织女-代理模式/11.3-站长统计.js
@@ -0,0 +1,24 @@
+/**
+ * @Author liming
+ * @Date 2024/6/13 11:59
+ **/
+
+//统计代理
+var Count = (function(){
+    //缓存图片
+    var img = new Image()
+    //返回统计函数
+    return function (param){
+        //统计请求字符串
+        var str = 'http://www.count.com/a.gif'
+        //拼接请求字符串
+        for (var i in param){
+            str += '&' + i + '=' + param[i]
+        }
+        //发送统计请求
+        _img.src = str
+    }
+})()
+
+//测试用例，统计num
+Count({num:10})
